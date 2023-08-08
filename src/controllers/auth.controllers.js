ff72ffd7741d8c3b9594da67ec7a0b97881595a1
GDMeira <gabrieldega970@gmail.com>
2023-08-07 23:12:42 -0300

    test: testando proteção de push na main 2.

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,5 @@
+
+
+export async function postUser(req, res) {
+    res.send('Created.');
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { stringStripHtml } from "../middlewares/stringStripHtmlValidation.js";
+import { postUser } from "../controllers/auth.controllers.js";
 
 
 const authRouter = Router()
 
-
+authRouter.post('/signup', stringStripHtml, postUser);
 
 export default authRouter
